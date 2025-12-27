7f3154c7b5aeaa09b8d0cfc6c2330fe64cd5cade
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2025-12-27 10:49:55 +0500

    cors ning joyi o'zgardi

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,6 @@ import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle("O'quvchilar uchun fake ma'lumotlardan iborat bo'lgan Server Sayt")
-    .setDescription("Barcha ma'lumotlar nazorat qilinmaydi")
-    .setVersion('1.0')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
   app.enableCors({
     origin: [
       'http://localhost:5173',
@@ -23,6 +15,14 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   })
+
+  const config = new DocumentBuilder()
+    .setTitle("O'quvchilar uchun fake ma'lumotlardan iborat bo'lgan Server Sayt")
+    .setDescription("Barcha ma'lumotlar nazorat qilinmaydi")
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT || 3003, '0.0.0.0');
 
 }
