5410f4a1c3d455a3b858a9498aaa653b14c900ca
IxtiyorSadullayev <sadullayevixtiyor2200@gmail.com>
2025-12-27 11:00:04 +0500

    cors moduli qo'shildi

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger'
-
+import * as cors from 'cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors: true});
+  const app = await NestFactory.create(AppModule);
   // app.enableCors({
   //   origin: [
   //     'http://localhost:5173',
@@ -15,6 +15,13 @@ async function bootstrap() {
   //   allowedHeaders: ['Content-Type', 'Authorization'],
   //   credentials: true,
   // })
+
+  app.use(
+    cors({
+      origin: ['http://localhost:5173', 'http://localhost:5173'],
+      methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
+    })
+  )
 
   const config = new DocumentBuilder()
     .setTitle("O'quvchilar uchun fake ma'lumotlardan iborat bo'lgan Server Sayt")
